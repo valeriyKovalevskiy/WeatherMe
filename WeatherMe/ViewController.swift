edc95c097c8fdb6b1b60cbfa6a31cc2e185027d2
Valeriy Kovalevskiy <v.kovalevskiy@vironit.co.uk>
2020-08-17 08:02:13 +0300

    Initial Commit

---
+++ WeatherMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherMe
+//
+//  Created by Valeriy Kovalevskiy on 8/17/20.
+//  Copyright © 2020 Valeriy Kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
