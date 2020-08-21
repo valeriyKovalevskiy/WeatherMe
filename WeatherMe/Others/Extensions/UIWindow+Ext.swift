ea0721d3d55615b290997eaabe53e0c1d7ec6f61
Valeriy Kovalevskiy <v.kovalevskiy@vironit.co.uk>
2020-08-21 18:58:52 +0300

    added blur background to SVProgressHUD alert

---
+++ WeatherMe/Others/Extensions/UIWindow+Ext.swift
@@ -0,0 +1,35 @@
+//
+//  UIWindow+Ext.swift
+//  WeatherMe
+//
+//  Created by Valeriy Kovalevskiy on 8/21/20.
+//  Copyright © 2020 Valeriy Kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+extension UIWindow {
+
+    func visibleViewController() -> UIViewController? {
+        if let rootViewController: UIViewController = self.rootViewController {
+            return UIWindow.getVisibleViewControllerFrom(vc: rootViewController)
+        }
+        return nil
+    }
+
+    static func getVisibleViewControllerFrom(vc:UIViewController) -> UIViewController {
+        if let navigationController = vc as? UINavigationController,
+            let visibleController = navigationController.visibleViewController  {
+            return UIWindow.getVisibleViewControllerFrom( vc: visibleController )
+        } else if let tabBarController = vc as? UITabBarController,
+            let selectedTabController = tabBarController.selectedViewController {
+            return UIWindow.getVisibleViewControllerFrom(vc: selectedTabController )
+        } else {
+            if let presentedViewController = vc.presentedViewController {
+                return UIWindow.getVisibleViewControllerFrom(vc: presentedViewController)
+            } else {
+                return vc
+            }
+        }
+    }
+}
--- WeatherMe/Others/Managers/ProgressManager.swift
+++ WeatherMe/Others/Managers/ProgressManager.swift
@@ -15,14 +15,72 @@ class ProgressManager {
     func showLoader(with status: String? = nil) {
         SVProgressHUD.setFont(UIFont(name: "SFUIText-Light", size: 23)!)
         SVProgressHUD.show(withStatus: status)
+        addBlurEffect()
     }
     
     func dismissLoader(completion: SVProgressHUDDismissCompletion? = nil) {
-        SVProgressHUD.dismiss(completion: completion)
+        SVProgressHUD.dismiss { [weak self] in
+            completion?()
+            self?.removeBlurEffect()
+        }
     }
     
     func dismissWithError(_ error: String) {
         SVProgressHUD.setFont(UIFont(name: "SFUIText-Light", size: 23)!)
-        SVProgressHUD.showError(withStatus: error)
+        SVProgressHUD.showDismissableError(with: error) { [weak self] in
+            self?.removeBlurEffect()
+        }
+    }
+
+    //TODO: - Move to another file
+    //MARK: - Visual effect
+    fileprivate let blurVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+    
+    fileprivate func addBlurEffect() {
+        if let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first {
+            if let topViewController = window.visibleViewController() {
+                blurVisualEffectView.frame = topViewController.view.bounds
+                topViewController.view.addSubview(blurVisualEffectView)
+            }
+        }
+    }
+    
+    fileprivate func removeBlurEffect() {
+        blurVisualEffectView.removeFromSuperview()
     }
 }
+
+extension SVProgressHUD {
+    
+    public static func showDismissableError(with status: String, completion: (() -> Void)? = nil) {
+        let nc = NotificationCenter.default
+        nc.addObserver(
+            self, selector: #selector(hudTapped(_:)),
+            name: NSNotification.Name.SVProgressHUDDidReceiveTouchEvent,
+            object: nil
+        )
+        nc.addObserver(
+            self, selector: #selector(hudDisappeared(_:)),
+            name: NSNotification.Name.SVProgressHUDWillDisappear,
+            object: nil
+        )
+        SVProgressHUD.showError(withStatus: status)
+        SVProgressHUD.setDefaultMaskType(.clear)
+        completion?()
+    }
+    
+    @objc
+    private static func hudTapped(_ notification: Notification) {
+        SVProgressHUD.dismiss()
+        SVProgressHUD.setDefaultMaskType(.none)
+    }
+    
+    @objc
+    private static func hudDisappeared(_ notification: Notification) {
+        let nc = NotificationCenter.default
+        nc.removeObserver(self, name: NSNotification.Name.SVProgressHUDDidReceiveTouchEvent, object: nil)
+        nc.removeObserver(self, name: NSNotification.Name.SVProgressHUDWillDisappear, object: nil)
+        SVProgressHUD.setDefaultMaskType(.none)
+    }
+}
+
